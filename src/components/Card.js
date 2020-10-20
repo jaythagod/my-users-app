bab3beaa12b866013fe6c9eee3ae27dd1b77f702
jtG <jude.gyimah87@gmail.com>
2020-10-21 01:32:27 +0200

    style clean up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import Button from './Button';
+import CardDetails from './CardDetails';
 import './Card.css';
 
 
 const Card = ({ id,name,uname,website }) =>{
     const firstLetter = name[0];
     return (
-        <div className='tc dib br3 pa2 ma2 grow bw2 shadow-5 w-25 bg-white'>
-            <div className="br-100 h3 w3 dib profileImage">{firstLetter}</div>
+        <div className='tc br3 pa2 ma2 grow bw2 shadow-5 w-25 bg-white'>
+            <div className="tc pa2 br-100 h3 w3 dib profileImage">{firstLetter}</div>
             <div>
-                <h4>{name}</h4>
-                <p>{`@`+uname}</p>
-                <Link to={`http://`+website}><p>{`http://`+website}</p></Link>
+                <CardDetails name={name} uname={uname} website={website} />
                 <Button id={id} />
             </div>
         </div>
