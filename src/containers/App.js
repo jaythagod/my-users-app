9f198fefbf67622b6ece9431a6d61f222cea1e42
jtG <jude.gyimah87@gmail.com>
2020-10-20 23:47:40 +0200

    minor refactoring and styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
+import React,  { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import UserDetails from './UserDetails';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 
-class App extends Component{
-  constructor(){
-      super();
-      this.state = {
-          allusers: []
-      }
-  }
+const App = ()=>{
+  const [allUsers, loadAllUsers] = useState([]);
 
-  componentDidMount(){
-      fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response =>{
-          return response.json();
-      })
-      .then(users =>{
-          this.setState({allusers: users});
-      });
-  }
+  useEffect(() =>{
+    const fetchAllUsers = async () =>{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const resJson = await response.json();
+        loadAllUsers(resJson);
+    }
 
-
-  render(){
-    const myUsers = this.state.allusers;
+    fetchAllUsers();
+    },[]);
 
     return (
       <Router>
-        <div className="tc">
+      <div className="tl fl w-20 pa2"></div>
+        <div className="tc fl w-60 pa2">
           <Switch>
-            <Route path="/" exact render={(props)=>(<CardList userinfo={ myUsers }/>)}/>
+            <Route path="/" exact render={(props)=>(<CardList userinfo={ allUsers }/>)}/>
             <Route path="/UserDetails/:id" render={()=> <UserDetails/>} />
           </Switch>
         </div>
+        <div className="tr fl w-20 pa2"></div>
       </Router>
     );
-  } 
+  
 }
 
 export default App;
